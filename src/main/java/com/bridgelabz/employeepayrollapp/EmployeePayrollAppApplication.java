a7e865e572ae1584717e45875548e7124617eeb2
Adityachess <89146150+Adityachess@users.noreply.github.com>
2021-12-27 15:22:29 +0530

    Added:using Lombok library for logging.

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -2,9 +2,12 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
