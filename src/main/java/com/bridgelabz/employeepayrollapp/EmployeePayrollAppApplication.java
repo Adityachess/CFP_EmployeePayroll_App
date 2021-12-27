c24fd0e69a629e8f8d249733997388c51a987e03
Adityachess <89146150+Adityachess@users.noreply.github.com>
2021-12-27 15:06:21 +0530

    Added:Using lombok library to auto generate getters and setters for the DTO.

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class EmployeePayrollAppApplication {
--- src/main/java/com/bridgelabz/employeepayrollapp/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/ResponseDTO.java
@@ -1,35 +1,26 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.*;
+
+
+@Data
+@AllArgsConstructor
 public class ResponseDTO {
 
 	private String message;
 	private Object data;
-
+/**	
 	public ResponseDTO(String message, Object data) {
 		super();
 		this.message = message;
 		this.data = data;
+		
 	}
+	*/
+	
+	
 
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public Object getData() {
-		return data;
-	}
-
-	public void setData(Object data) {
-		this.data = data;
-	}
-
-	@Override
-	public String toString() {
-		return "message=" + message + ", data=" + data ;
-	}
-
+	
 }
