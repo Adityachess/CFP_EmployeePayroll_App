1fab2baf7f7bf1779c9509e7533167d4075c980b
Adityachess <89146150+Adityachess@users.noreply.github.com>
2021-12-27 17:17:24 +0530

    Added:Creating database setting as environment variable.

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -15,5 +15,7 @@ public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
 		
 		log.info("Employee Payroll App Started in {} Environment",context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is {} Environment",context.getEnvironment().getProperty("spring.datasource.username"));
+
 	}
 }
