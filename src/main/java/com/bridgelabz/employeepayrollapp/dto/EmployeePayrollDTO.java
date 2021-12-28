8ca5a38a94d490c7dfc5a6efa21e0b9fbe3ae92f
Adityachess <89146150+Adityachess@users.noreply.github.com>
2021-12-28 20:05:06 +0530

    Added:Inserting remaining propertieslike gender and startdate to dto and model.

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,23 +1,34 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.ToString;
+
+@ToString
 public class EmployeePayrollDTO {
-	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\$]{2,}$",message = "Invalid Employee name")
-    @NotEmpty(message = "Employee name cannot null")
+
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee Name is invalid")
 	public String name;
-	@Min(value = 500, message = "min wage should be more than 500")
-    public long salary;
 
-    public EmployeePayrollDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
+	@Min(value = 500, message = "Min Wage should be more than 500")
+	public long salary;
 
-    @Override
-    public String toString() {
-        return "Name=" + name + " Salary=" + salary;
-    }
+	public String gender;
+
+	@JsonFormat(pattern = "dd MMM yyyy")
+	public LocalDate startDate;
+
+	public String note;
+
+	public String profilePic;
+
+	public List<String> departments;
+
 }
